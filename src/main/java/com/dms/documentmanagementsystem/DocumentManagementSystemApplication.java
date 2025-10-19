e1bf2434326bc22b6769a1f26396dc042a437c10
Haidy Yasser <haidyasser3@gmail.com>
2025-10-19 13:48:15 +0300

    renaming

--- src/main/java/com/dms/documentmanagementsystem/DocumentManagementSystemApplication.java
+++ src/main/java/com/dms/documentmanagementsystem/DocumentManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.documentmanagementsystem;
+package com.dms.documentmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dms/documentmanagementsystem/DocumentManagementSystemApplicationTests.java
+++ src/test/java/com/dms/documentmanagementsystem/DocumentManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.documentmanagementsystem;
+package com.dms.documentmanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
